63fffb34a5d8b721409e81fbab9967cb6de469f9
Danilo Barion Nogueira <danilo.barion@vagas.com.br>
2017-10-27 18:18:12 -0200

    Finish implementation of Brazilian CPF generator.

--- lib/faker/brazilian_docs.rb
+++ lib/faker/brazilian_docs.rb
@@ -1,10 +1,9 @@
 module Faker
   class BrazilianDocs < Base
 
-    CPF_MULTIPLIERS = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+    CPF_FIRST_MULTIPLIERS = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+    CPF_SECOND_MULTIPLIERS = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
     CPF_MAGIC_NUMBER = 11
-    @cpf_number_array = nil
-    @first_verification_digit = nil
 
     class << self
 
@@ -21,20 +20,21 @@ module Faker
       private
 
       def _default_steps
-        _random_cpf_number_array
+        @cpf_first_array = Faker::BrazilianDocs.numerify("#########").scan(/./).map { |x| x.to_i }
+        @first_verification_digit = nil
+        @second_verification_digit = nil
         _calculate_verification_digits
       end
 
-      def _random_cpf_number_array
-        Faker::BrazilianDocs.numerify("#########").scan(/./).map { |x| x.to_i }
-      end
-
       def _calculate_verification_digits
-        @cpf_number_array = _calculate_first_verification_digit
+        @first_verification_digit = _calculate_first_digit
+        @cpf_first_array.push(@first_verification_digit)
+        @second_verification_digit = _calculate_second_digit
+        @cpf_first_array.push(@second_verification_digit)
       end
 
-      def _calculate_first_verification_digit
-        remainder = _sum_multiplied_digits % CPF_MAGIC_NUMBER
+      def _calculate_first_digit
+        remainder = _sum_digits % CPF_MAGIC_NUMBER
 
         if remainder < 2
           @first_verification_digit = 0
@@ -43,22 +43,42 @@ module Faker
         end
       end
 
-      def _sum_multiplied_digits
+      def _calculate_second_digit
+        remainder = _sum_digits_again % CPF_MAGIC_NUMBER
+
+        if remainder < 2
+          @second_verification_digit = 0
+        else
+          @second_verification_digit = CPF_MAGIC_NUMBER - remainder
+        end
+      end
+
+      def _sum_digits
         _multiple_each_digit.reduce(:+)
       end
 
+      def _sum_digits_again
+        _multiple_each_digit_again.reduce(:+)
+      end
+
       def _multiple_each_digit
-        _random_cpf_number_array.each_with_index do |n, index|
-          n * CPF_MULTIPLIERS[index]
+        @cpf_first_array.each_with_index.map do |n, index|
+          n * CPF_FIRST_MULTIPLIERS[index]
+        end
+      end
+
+      def _multiple_each_digit_again
+        @cpf_first_array.each_with_index.map do |n, index|
+          n * CPF_SECOND_MULTIPLIERS[index]
         end
       end
 
       def _return_valid_cpf_number
-        @cpf_number_array.join("").to_i
+        @cpf_first_array.join("").to_i
       end
 
       def _return_invalid_cpf_number
-        last_digit = @cpf_number_array.last
+        last_digit = @cpf_first_array.last
 
         if last_digit < 9
           last_digit = last_digit + 1
@@ -66,8 +86,8 @@ module Faker
           last_digit = last_digit - 1
         end
 
-        @cpf_number_array[10] = last_digit
-        @cpf_number_array.join("").to_i
+        @cpf_first_array[10] = last_digit
+        @cpf_first_array.join("").to_i
       end
     end
 
